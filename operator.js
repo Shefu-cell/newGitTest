498d818f01463456eb1bdc5f0a8fca35b1b60ec1
shefu-cell <shefalikumari103@gmail.com>
2019-07-26 14:17:56 +0530

    add file

---
+++ operator.js
@@ -0,0 +1,15 @@
+function favorteFood(...food)
+{
+    console.log(food);
+}
+console.log('favorteFood of person A');
+let a =['rajma chaval','chole bhature','ice cream','sweets'];
+favorteFood(...a);
+console.log('favorteFood of person B');
+let b =['chole bhature','panner','sweets'];
+favorteFood(...b);
+console.log('favorteFood of person C');
+let c =['Raj chachri' ,'chole bhature ','suops','sweets'];
+favorteFood(...c);
+
+
